56a378556c9284d5bdac6ab3ce07dacc295dd4fe
dwiki.arlan <dwiki.arlan@ruangguru.com>
2022-11-24 14:33:01 +0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/index";
+
+export default function Homes() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>Nice</div>
+    </div>
+  );
+}
